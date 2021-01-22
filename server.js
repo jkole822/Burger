da324bc55d8a10ee8aa748fb9fd82a75e238672b
Kole Gasior <jkole822@gmail.com>
2021-01-22 16:32:02 -0500

    add middleware to serve static files and parse req.body

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 const routes = require("./controllers/burgers_controller");
-
 app.use(routes);
 
 app.listen(PORT, () => {
