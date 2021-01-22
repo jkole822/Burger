d43f79fb3999257a1f23dd60fb68fda94d25accc
Kole Gasior <jkole822@gmail.com>
2021-01-22 16:19:47 -0500

    configure server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+const routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
