9aced10be4c4036a3dcb15299258b5f66a5ca710
Kole Gasior <jkole822@gmail.com>
2021-01-22 16:58:24 -0500

    add get, post, and patch routes to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,35 @@ const express = require("express");
 const burger = require("../models/burger");
 
 const router = express.Router();
+
+router.get("/api/burgers", (req, res) => {
+	burger.fetch((err, data) => {
+		if (err) return res.status(500).end();
+
+		console.log(`Fetching burgers: ${data}`);
+
+		hbsObject = {
+			burgers: data,
+		};
+
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/api/burgers", (req, res) => {
+	burger.create(req.body.burger, (err, data) => {
+		if (err) return res.status(500).end();
+		console.log(`Creating burgers: ${data}`);
+		res.status(201).end();
+	});
+});
+
+router.patch("/api/burgers/:id", (req, res) => {
+	burger.update(req.params.id, (err, data) => {
+		if (err) return res.status(500).end();
+		console.log(`Updating burgers: ${data}`);
+		res.status(200).end();
+	});
+});
+
+module.exports = router;
