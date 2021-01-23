c5718076464fb2690a00ac996839afe7df451323
Kole Gasior <jkole822@gmail.com>
2021-01-22 20:22:23 -0500

    correct typos; edit console calls

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,24 +3,23 @@ const burger = require("../models/burger");
 
 const router = express.Router();
 
-router.get("/api/burgers", (req, res) => {
+router.get("/", (req, res) => {
 	burger.fetch((err, data) => {
 		if (err) return res.status(500).end();
-
-		console.log(`Fetching burgers: ${data}`);
-
+		console.log("Fetching burgers: ");
+		console.table(data);
 		hbsObject = {
 			burgers: data,
 		};
-
 		res.render("index", hbsObject);
 	});
 });
 
 router.post("/api/burgers", (req, res) => {
-	burger.create(req.body.burger, (err, data) => {
+	burger.create(req.body.burger_name, (err, data) => {
 		if (err) return res.status(500).end();
-		console.log(`Creating burgers: ${data}`);
+		console.log("Creating burgers:");
+		console.table(data);
 		res.status(201).end();
 	});
 });
@@ -28,7 +27,8 @@ router.post("/api/burgers", (req, res) => {
 router.patch("/api/burgers/:id", (req, res) => {
 	burger.update(req.params.id, (err, data) => {
 		if (err) return res.status(500).end();
-		console.log(`Updating burgers: ${data}`);
+		console.log("Updating burgers:");
+		console.table(data);
 		res.status(200).end();
 	});
 });
