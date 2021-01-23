f52c463989326429ee3f5ebc890db1205b1b08cc
Kole Gasior <jkole822@gmail.com>
2021-01-23 15:16:42 -0500

    put location reload into callback

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,26 +2,28 @@ $("#create-burger").submit(() => {
 	const data = { burger_name: $("#burger-name").val().trim() };
 
 	if (data.burger_name) {
-		$.post("/api/burgers", data);
-
-		location.reload();
+		$.post("/api/burgers", data, () => location.reload());
 	}
 });
 
 $(".devour-button").click(function () {
 	const id = $(this).attr("data-id");
 
-	$.ajax(`/api/burgers/${id}`, {
-		type: "PATCH",
-	});
-
-	location.reload();
+	$.ajax(
+		`/api/burgers/${id}`,
+		{
+			type: "PATCH",
+		},
+		() => location.reload()
+	);
 });
 
 $("#clear-button").click(() => {
-	$.ajax("/api/burgers", {
-		type: "DELETE",
-	});
-
-	location.reload();
+	$.ajax(
+		"/api/burgers",
+		{
+			type: "DELETE",
+		},
+		() => location.reload()
+	);
 });
